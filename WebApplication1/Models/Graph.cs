c97b4120b5ffc587748030fab359acd0c373c0ea
Joao Miguel <joaomiguelgalvaoferrazd@gmail.com>
2026-06-01 17:55:18 -0400

    commit: inicial

---
+++ WebApplication1/Models/Graph.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+
+
+namespace PacMan.Models
+
+{
+    public class Graph
+    {
+        public Dictionary<int, List<int>> AdjacencyList { get; set; }
+
+        public Graph()
+        {
+            AdjacencyList = new Dictionary<int, List<int>>();
+        }
+
+        public void AddEdge(int source, int destination)
+        {
+            if(!AdjacencyList.ContainsKey(source))
+            {
+                AdjacencyList[source] = new List<int>();
+            }
+
+            if(!AdjacencyList.ContainsKey(destination))
+            {
+                AdjacencyList[destination] = new List<int>();
+            }
+
+            AdjacencyList[source].Add(destination);
+            AdjacencyList[destination].Add(source);
+        }
+
+        public void PrintGraph()
+        {
+            foreach (var vertex in AdjacencyList)
+            {
+                Console.Write($"{vertex.Key} -> ");
+
+                foreach(var neighbor in vertex.Value)
+                {
+                    Console.Write($"{neighbor} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ WebApplication1/Services/GraphTestService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using PacMan.Models;
+
+namespace PacMan.Services
+{
+    public class GraphTestService
+    {
+        public void Run()
+        {
+            Graph graph = new Graph();
+
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(2, 4);
+
+            graph.PrintGraph();
+
+        }
+    }
+}
