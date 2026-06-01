7da01248cc12d29b5876b9f2f02280919a5fd53d
Joao Miguel <joaomiguelgalvaoferrazd@gmail.com>
2026-06-01 18:21:14 -0400

    models: node.cs criado

--- WebApplication1/Models/Node.cs
+++ WebApplication1/Models/Node.cs
@@ -1,6 +1,21 @@
-﻿namespace PacMan.Models
+﻿using System.Collections.Generic;
+
+namespace PacMan.Models
 {
     public class Node
     {
+        public int Id { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public List<int> Neighbors { get; set; }
+
+        public Node(int id, int row, int col)
+        {
+            Id = id;
+            Row = row;
+            Col = col;
+            Neighbors = new List<int>();
+        }
+
     }
 }
