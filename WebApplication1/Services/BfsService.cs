fc0131d444e4a7a7dbc628a4124cd9e0c1e170ac
Joao Miguel <joaomiguelgalvaoferrazd@gmail.com>
2026-06-01 21:54:43 -0400

    Services: BfsService feito(movimentação do fantasma no labirinto)

---
+++ WebApplication1/Services/BfsService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using PacMan.Models;
+
+namespace PacMan.Services
+{
+    public class BfsService
+    {
+        public List<int> FindPath(Graph graph, int start, int end)
+        {
+
+            Queue<int> queue = new Queue<int>();
+            HashSet<int> visited = new HashSet<int>();
+            Dictionary<int, int> cameFrom = new Dictionary<int, int>();
+
+            queue.Enqueue(start);
+            visited.Add(start);
+
+            while (queue.Count > 0)
+            {
+                int atual = queue.Dequeue();
+
+                if (atual == end)
+                {
+                    List<int> path = new List<int>();
+                    int passo = end;
+
+                   while(passo != start)
+                    {
+                        path.Add(passo);
+                        passo = cameFrom[passo];
+                    }
+
+                    path.Add(start);
+                    path.Reverse();
+                    return path;
+
+                }
+
+                foreach(int neighbor in graph.AdjacencyList[atual])
+                {
+                    if(!visited.Contains(neighbor))
+                    {
+                        visited.Add(neighbor);
+                        cameFrom[neighbor] = atual;
+                        queue.Enqueue(neighbor);
+                    }
+                }
+
+            }
+
+            return new List<int>(); // retorna lista vazia
+        }
+    }
+}
--- WebApplication1/Services/GraphTestService.cs
+++ WebApplication1/Services/GraphTestService.cs
@@ -8,20 +8,26 @@ namespace PacMan.Services
         public void Run()
         {
             MazeService mazeService = new MazeService();
+            BfsService bfsService = new BfsService();
 
             int[,] maze = {
-
-                {1, 1, 1, 1, 1 },
-                {1, 0, 0, 0, 1 },
-                {1, 0, 1, 0, 1 },
-                {1, 0, 0, 0, 1 },
-                {1, 1 ,1, 1, 1 }
-
-            };
+        { 1, 1, 1, 1, 1 },
+        { 1, 0, 0, 0, 1 },
+        { 1, 0, 1, 0, 1 },
+        { 1, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 1 }
+    };
 
             Graph graph = mazeService.BuildGraph(maze);
-            graph.PrintGraph();
 
+            // fantasma no nó 16, Pac-Man no nó 8
+            List<int> path = bfsService.FindPath(graph, 16, 8);
+
+            Console.WriteLine("Caminho do fantasma:");
+            foreach (int no in path)
+            {
+                Console.Write($"{no} → ");
+            }
         }
     }
 }
