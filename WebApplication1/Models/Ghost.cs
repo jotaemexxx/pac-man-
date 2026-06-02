c5aea8dc3c96fd1e6f52a9a4f0395367708109ee
Joao Miguel <joaomiguelgalvaoferrazd@gmail.com>
2026-06-02 16:59:43 -0400

    models: player e ghost criados

---
+++ WebApplication1/Models/Ghost.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PacMan.Models
+{
+    public class Ghost
+    {
+
+        public Node CurrentNode { get; set; }
+        public string Color { get; set; }
+
+        public Ghost(Node startNode, string color)
+        {
+            CurrentNode = startNode;
+            Color = color;
+        }
+
+
+
+    }
+}
---
+++ WebApplication1/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+
+namespace PacMan.Models
+{
+    public class Player
+    {
+        public Node CurrentNode { get; set; }
+        public int Score { get; set; }
+
+        public Player(Node StartNode)
+        {
+            CurrentNode = StartNode;
+            Score = 0;
+
+        }
+
+
+    }
+}
